1656b21c0cefbbe71488059a2caeb002eab0161b
Rustam Uzairov <mr.uzairov@gmail.com>
2023-08-10 13:20:56 +0300

    ...

--- relang_env_packagemanager.sh
+++ relang_env_packagemanager.sh
@@ -20,8 +20,8 @@ RV='\u001b[7m'
 
 THIS_REPO_PATH="$(dirname "$(realpath "$0")")"
 # THIS_REPO_PATH=$HOME/REPOS/reinst
-# DOT_CFG_PATH=$THIS_REPO_PATH/config
-# DOT_HOME_PATH=$THIS_REPO_PATH/home
+DOT_CFG_PATH=$THIS_REPO_PATH/config
+DOT_HOME_PATH=$THIS_REPO_PATH/home
 USR_CFG_PATH=$HOME/.config
 SRC_DIR=$HOME/src
 # FONT_DIR=$HOME/.local/share/fonts
@@ -106,42 +106,42 @@ function install_packages {
 	sudo ${PACKAGER} install -yq ${DEPENDENCIES}
 }
 
+# перед создание линков делает бекапы только тех пользовательских конфикураций,
+# файлы которых есть в ./config ./home
 function back_sym {
-	# перед создание линков делает бекапы только тех пользовательских конфикураций,
-	# файлы которых есть в ./config ./home
-	mkdir -p "$USR_CFG_PATH"
-	echo -e "${RV}${YELLOW} Backing up existing files... ${RC}"
-	for config in $(command ls ${DOT_CFG_PATH}); do
-		if configExists "${USR_CFG_PATH}/${config}"; then
-			echo -e "${YELLOW}Moving old config ${USR_CFG_PATH}/${config} to ${USR_CFG_PATH}/${config}.old${RC}"
-			if ! mv "${USR_CFG_PATH}/${config}" "${USR_CFG_PATH}/${config}.old"; then
+	# mkdir -p "$USR_CFG_PATH"
+	# echo -e "${RV}${YELLOW} Backing up existing files... ${RC}"
+	# for config in $(command ls ${DOT_CFG_PATH}); do
+	# 	if configExists "${USR_CFG_PATH}/${config}"; then
+	# 		echo -e "${YELLOW}Moving old config ${USR_CFG_PATH}/${config} to ${USR_CFG_PATH}/${config}.old${RC}"
+	# 		if ! mv "${USR_CFG_PATH}/${config}" "${USR_CFG_PATH}/${config}.old"; then
+	# 			echo -e "${RED}Can't move the old config!${RC}"
+	# 			exit 1
+	# 		fi
+	# 		echo -e "${WHITE} Remove backups with 'rm -ir ~/.*.old && rm -ir ~/.config/*.old' ${RC}"
+	# 	fi
+	# 	echo -e "${GREEN}Linking ${DOT_CFG_PATH}/${config} to ${USR_CFG_PATH}/${config}${RC}"
+	# 	if ! ln -snf "${DOT_CFG_PATH}/${config}" "${USR_CFG_PATH}/${config}"; then
+	# 		echo echo -e "${RED}Can't link the config!${RC}"
+	# 		exit 1
+	# 	fi
+	# done
+
+	for config in $(command ls ${DOT_HOME_PATH}); do
+		if configExists "$HOME/.${config}"; then
+			echo -e "${YELLOW}Moving old config ${HOME}/.${config} to ${HOME}/.${config}.old${RC}"
+			if ! mv "${HOME}/.${config}" "${HOME}/.${config}.old"; then
 				echo -e "${RED}Can't move the old config!${RC}"
 				exit 1
 			fi
 			echo -e "${WHITE} Remove backups with 'rm -ir ~/.*.old && rm -ir ~/.config/*.old' ${RC}"
 		fi
-		echo -e "${GREEN}Linking ${DOT_CFG_PATH}/${config} to ${USR_CFG_PATH}/${config}${RC}"
-		if ! ln -snf "${DOT_CFG_PATH}/${config}" "${USR_CFG_PATH}/${config}"; then
+		echo -e "${GREEN}Linking ${DOT_HOME_PATH}/${config} to ${HOME}/.${config}${RC}"
+		if ! ln -snf "${DOT_HOME_PATH}/${config}" "${HOME}/.${config}"; then
 			echo echo -e "${RED}Can't link the config!${RC}"
 			exit 1
 		fi
 	done
-
-	# for config in $(ls ${DOT_HOME_PATH}); do
-	# 	if configExists "$HOME/.${config}"; then
-	# 		echo -e "${YELLOW}Moving old config ${HOME}/.${config} to ${HOME}/.${config}.old${RC}"
-	# 		if ! mv "${HOME}/.${config}" "${HOME}/.${config}.old"; then
-	# 			echo -e "${RED}Can't move the old config!${RC}"
-	# 			exit 1
-	# 		fi
-	#		  echo -e "${WHITE} Remove backups with 'rm -ir ~/.*.old && rm -ir ~/.config/*.old' ${RC}"
-	# 	fi
-	# 	echo -e "${GREEN}Linking ${DOT_HOME_PATH}/${config} to ${HOME}/.${config}${RC}"
-	# 	if ! ln -snf "${DOT_HOME_PATH}/${config}" "${HOME}/.${config}"; then
-	# 		echo echo -e "${RED}Can't link the config!${RC}"
-	# 		exit 1
-	# 	fi
-	# done
 
 }
 
