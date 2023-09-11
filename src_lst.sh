cc40c7a5a3b8e2dc267a48cfd18f6c57c9d92950
Rustam Uzairov <mr.uzairov@gmail.com>
2023-09-12 00:00:30 +0300

    **apt-key list rep

--- src_lst.sh
+++ src_lst.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 this_dir_path="$(dirname "$(realpath "$0")")"
-confif_dir=etc/apt
-source_lists_dir="/$confif_dir"/sources.list.d
-keyrings_dirs="/$confif_dir"/keyrings
+confif_dirs=etc/apt
+source_lists_dirs="$confif_dirs"/sources.list.d
+keyrings_dirs="$confif_dirs"/keyrings
 
-sudo mkdir -p $source_lists_dir $keyrings_dirs
+# sudo mkdir -p $source_lists_dirs $keyrings_dirs
 
-for s in $(command ls "$this_dir_path$source_lists_dir"); do
-	sudo cp "$this_dir_path$source_lists_dir/$s" "$source_lists_dir/$s"
-done
+sudo ln -svnf "$this_dir_path/$source_lists_dirs" "/$source_lists_dirs"
 
-for k in $(command ls "$this_dir_path$keyrings_dirs"); do
-	sudo cp "$this_dir_path$keyrings_dirs/$k" "$keyrings_dirs/$k"
-done
-sudo chown ru:ru -R $keyrings_dirs $source_lists_dir
+sudo cp -r "$this_dir_path/$keyrings_dirs" "/$keyrings_dirs"
+
+# sudo chown root:root -R $keyrings_dirs $source_lists_dir
