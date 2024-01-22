10f9e5b3aa2b6ff519d9f85f0e8ae6c883db1198
Rustam Uzairov <mr.uzairov@gmail.com>
2024-01-22 17:45:36 +0000

    fix name gpg/asc

--- bin/keyrings.sh
+++ bin/keyrings.sh
@@ -7,23 +7,29 @@
 sources_keyrings_dir=$1
 destination_keyrings_dir=$2
 
-# apt_dir=etc/apt
+# apt_dir=/etc/apt
 # sources_dir="$apt_dir"/sources.list.d
 # keyrings_dir="$apt_dir"/keyrings
 
 # this_dir="$(dirname "$(realpath "$0")")"
 # destination_keyrings_dir=$this_dir/test
-# mkdir -p "$destination_keyrings_dir"
+mkdir -p "$destination_keyrings_dir"
 
 keys=$(command ls "$sources_keyrings_dir")
+# for k in $keys; do
+# 	name="${k%.*}"
+# 	# echo -E "$name"
+# done
 
 function update_apt_keyrings() {
 	for k in $keys; do
-		recv_keys=$(gpg -k --no-default-keyring --keyring "$destination_keyrings_dir/$k" |
+		name="${k%.*}"
+		recv_keys=$(gpg -k --no-default-keyring --keyring "$sources_keyrings_dir/$name.gpg" |
 			grep -E "([0-9A-F]{40})" | tr -d " ")
-		echo -E "$recv_keys"
+		# echo -E "$name"
+		# echo -E "$recv_keys"
 		gpg --no-default-keyring \
-			--keyring "$sources_keyrings_dir/$k" \
+			--keyring "$sources_keyrings_dir/$name.gpg" \
 			--keyserver hkps://keyserver.ubuntu.com \
 			--recv-keys "$recv_keys"
 	done
@@ -38,18 +44,30 @@ function update_gpg_keyrings() {
 }
 # update_gpg_keyrings
 
+function to_bin_apt_keys() {
+	for k in $keys; do
+		name="${k%.*}"
+		command cat "$sources_keyrings_dir/$name.asc" | gpg --dearmor \
+			-o "$destination_keyrings_dir/$name.gpg"
+	done
+}
 
 function to_ascii_apt_keys() {
 	for k in $keys; do
-		gpg --export --armor --no-default-keyring --keyring "$sources_keyrings_dir/$k" -o "$destination_keyrings_dir/$k.asc"
+		name="${k%.*}"
+		gpg --export --armor --no-default-keyring \
+			--keyring "$sources_keyrings_dir/$name.gpg" \
+			-o "$destination_keyrings_dir/$name.asc"
 	done
 }
 to_ascii_apt_keys
+
 # echo -e "$1"
 # echo -e "$2"
+
+# gpg -k --no-default-keyring --keyring ./*.gpg
 
 # gpg --homedir /tmp --no-default-keyring --keyring "$destination_keyrings_dir/$name" \
 # gpg --list-keys --with-colons | awk -F: '/^fpr:/ { print $10 }'
 # gpg --no-default-keyring --keyring /etc/apt/keyrings/brave-browser-release.gpg --with-colons --fingerprint | awk -F: '/^fpr:/ { print $10 }'
 # gpg --no-default-keyring --keyring /etc/apt/keyrings/brave-browser-release.gpg --fingerprint | sed -n '/^\s/s/\s*//p'
-
