7d5ba39edb2d89106fc9a5b9f407257a2c4fe1f5
Rustam Uzairov <mr.uzairov@gmail.com>
2024-01-22 15:20:23 +0000

    update keyring functionatity

--- bin/keyrings.sh
+++ bin/keyrings.sh
@@ -1,23 +1,37 @@
 #!/bin/env bash
 
+# if [ $# -eq 0 ]; then # Для работы скрипта необходим входной параметр.
+# 	echo "Вызовите сценарий с параметрами: keyrings.sh sources_dir destination_dir "
+# fi
+
 # apt_dir=etc/apt
 # sources_dir="$apt_dir"/sources.list.d
 # keyrings_dir="$apt_dir"/keyrings
 
-sources_keyrings_dir=$1
-destination_keyrings_dir=$2
+this_dir="$(dirname "$(realpath "$0")")"
 
-mkdir -p "$destination_keyrings_dir"
+sources_keyrings_dir=$this_dir/keyrings
+
+# sources_keyrings_dir=$1
+# destination_keyrings_dir=$2
+
+# mkdir -p "$destination_keyrings_dir"
 keys=$(command ls "$sources_keyrings_dir")
 
-function update_keys() {
+function update_bin_keyrings() {
 	for k in $keys; do
-		sudo gpg --no-default-keyring \
+		recv_keys=$(gpg -k --no-default-keyring --keyring "$sources_keyrings_dir/$k" \
+      | grep -E "([0-9A-F]{40})" | tr -d " ")
+    echo -E "$recv_keys"
+		gpg --no-default-keyring \
 			--keyring "$sources_keyrings_dir/$k" \
-			--keyserver hkps://keyserver.ubuntu.com
-		# --recv-keys <fingerprint>
+			--keyserver hkps://keyserver.ubuntu.com \
+			--recv-keys "$recv_keys"
 	done
 }
+update_bin_keyrings
+
+
 # gpg --list-keys --with-colons | awk -F: '/^fpr:/ { print $10 }'
 # gpg --list-keys --with-colons | awk -F: '/^pub:/ { print $5 }'
 # gpg --no-default-keyring --keyring /etc/apt/keyrings/*.gpg --fingerprint
@@ -30,8 +44,7 @@ function to_ascii_apt_keys() {
 		gpg --export --armor --no-default-keyring --keyring "$sources_keyrings_dir/$k" -o "$destination_keyrings_dir/$k.key"
 	done
 }
-to_ascii_apt_keys
+
+# to_ascii_apt_keys
 # echo -e "$1"
 # echo -e "$2"
-
-
