5bb560b1723d133da711c52192a98d1baa19f8ff
Rustam Uzairov <mr.uzairov@gmail.com>
2024-01-31 15:57:43 +0000

    fix script

--- bin/keyrings.sh
+++ bin/keyrings.sh
@@ -4,38 +4,25 @@
 # 	echo "Вызовите сценарий с параметрами: keyrings.sh sources_dir destination_dir "
 # fi
 
-# apt_dir=/etc/apt
-# sources_dir="$apt_dir"/sources.list.d
-# keyrings_dir="$apt_dir"/keyrings
-
 # this_dir="$(dirname "$(realpath "$0")")"
-# destination_keyrings_dir=$this_dir/test
+# destination_dir=$this_dir/test
 
 sources_dir=$2
 destination_dir=$3
 
-mkdir -p "$destination_dir"
-
 keys=$(command ls "$sources_dir")
-# for k in $keys; do
-# 	name="${k%.*}"
-# 	# echo -E "$name"
-# done
 
 function update_apt_keyrings() {
 	for k in $keys; do
 		name="${k%.*}"
 		recv_keys=$(gpg -k --no-default-keyring --keyring "$sources_dir/$name.gpg" |
 			grep -E "([0-9A-F]{40})" | tr -d " ")
-		# echo -E "$name"
-		# echo -E "$recv_keys"
 		gpg --no-default-keyring \
 			--keyring "$sources_dir/$name.gpg" \
 			--keyserver hkps://keyserver.ubuntu.com \
 			--recv-keys "$recv_keys"
 	done
 }
-# update_apt_keyrings
 
 function update_gpg_keyrings() {
 	recv_keys=$(gpg -k --with-colons | awk -F: '/^fpr:/ { print $10 }')
@@ -43,47 +30,64 @@ function update_gpg_keyrings() {
 		gpg --keyserver keyserver.ubuntu.com --recv-keys "$r"
 	done
 }
-# update_gpg_keyrings
 
 function to_bin_apt_keys() {
+
+	if [ -d "$destination_dir" ]; then
+		sudo mv "$destination_dir" "$destination_dir.old"
+		sudo mkdir -p "$destination_dir"
+	else
+		sudo mkdir -p "$destination_dir"
+	fi
+
 	for k in $keys; do
 		name="${k%.*}"
-		# command cat "$sources_keyrings_dir/$name.asc" | gpg --dearmor \
-		# 	-o "$destination_dir/$name.gpg"
-    gpg --dearmor --yes -o \
-      "$destination_dir/$name.gpg" < "$sources_dir/$name.asc"
+		command cat "$sources_dir/$name.asc" | sudo gpg --dearmor \
+			-o "$destination_dir/$name.gpg"
+		# sudo gpg --dearmor --yes -o \
+		# 	"$destination_dir/$name.gpg" < "$sources_dir/$name.asc"
 	done
 }
 
-function to_ascii_apt_keys() {
+function to_asc_apt_keys() {
+	[ -d "$destination_dir" ] || mkdir -p "$destination_dir"
+	tmp_dir=$TMP
 	for k in $keys; do
 		name="${k%.*}"
-		gpg --export --armor --no-default-keyring \
-			--keyring "$sources_dir/$name.gpg" \
-			-o "$destination_dir/$name.asc"
+		if [ ! -f "$destination_dir/$name.asc" ]; then
+			gpg --export --armor --no-default-keyring \
+				--keyring "$sources_dir/$name.gpg" \
+				-o "$destination_dir/$name.asc"
+			# else
+			# 	gpg --export --armor --no-default-keyring \
+			# 		--keyring "$sources_dir/$name.gpg" \
+			# 		-o "$tmp_dir/$name.asc"
+			# if ! "$tmp_dir/$name.asc" -eq "$destination_dir/$name.asc" ;
+			#   diff "$tmp_dir/$name.asc" "$destination_dir/$name.asc"
+			# fi
+
+		fi
 	done
 }
 
 for i in "$@"; do
-  	if [[ "$i" = --* ]]; then
-      case $i in
-        "--to-asc")
-          to_ascii_apt_keys
-          ;;
-        "--to-bin")
-          to_bin_apt_keys
-          ;;
-        "--update-apt")
-          update_apt_keyrings
-          ;;
-        "--update-gpg")
-          update_gpg_keyrings
-          ;;
-      esac
-    fi
+	if [[ "$i" = --* ]]; then
+		case $i in
+		"--to-asc")
+			to_asc_apt_keys
+			;;
+		"--to-bin")
+			to_bin_apt_keys
+			;;
+		"--update-apt")
+			update_apt_keyrings
+			;;
+		"--update-gpg")
+			update_gpg_keyrings
+			;;
+		esac
+	fi
 done
-
-
 
 # echo -e "$1"
 # echo -e "$2"
